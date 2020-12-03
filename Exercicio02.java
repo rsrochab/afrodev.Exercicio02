3c66b9a274c673fdfe9c3ce1969f4ae931e2fc8d
rsrochab <rs.rochab@gmail.com>
2020-12-03 17:31:26 -0300

    Inserção de comentários
    Adicionados comentários ao código.

--- Exercicio02.java
+++ Exercicio02.java
@@ -1,11 +1,11 @@
 package Triangulo;
 import java.util.*;
-
+//Desenvolvido por Rodrigo Brito
 public class Exercicio02 {
 
 	
 	public static void main(String[] args) {
-		
+		//Criando objetos e instanciando o método da classe Triangulo
 		Triangulo t1 = new Triangulo();
 		Scanner scan = new Scanner(System.in);
 		t1.calculaBase(scan);
@@ -16,6 +16,7 @@ public class Exercicio02 {
 		Triangulo t3 = new Triangulo();
 		t3.calculaBase(scan);
 		
+		//Exibindo resultado final para o usuário
 		System.out.println("----------------------------");
 		System.out.println("RESULTADO FINAL");
 		System.out.println(" A área do primeiro triangulo é: " + t1.getTotal() + 
