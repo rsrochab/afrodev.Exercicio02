6a1114f144538da02cc00af631ee871d60ce6196
rsrochab <rs.rochab@gmail.com>
2020-12-03 17:22:46 -0300

    Atualização de conteudo.
    Nesta classe foi instanciado 3 triângulos para obtenção de dados do usuário de base e altura.
    A cada entrada de dados feita pelo usuário, é mostrado o resultado da área do triangulo e ao final do programa é exibido a área de todos os triângulos.

--- Exercicio02.java
+++ Exercicio02.java
@@ -1,5 +1,26 @@
 package Triangulo;
-//Desenvolvidor por Rodrigo Brito
+import java.util.*;
+
 public class Exercicio02 {
 
+	
+	public static void main(String[] args) {
+		
+		Triangulo t1 = new Triangulo();
+		Scanner scan = new Scanner(System.in);
+		t1.calculaBase(scan);
+		
+		Triangulo t2 = new Triangulo();
+		t2.calculaBase(scan);
+		
+		Triangulo t3 = new Triangulo();
+		t3.calculaBase(scan);
+		
+		System.out.println("----------------------------");
+		System.out.println("RESULTADO FINAL");
+		System.out.println(" A área do primeiro triangulo é: " + t1.getTotal() + 
+				" \n A área do segundo triangulo é: " + t2.getTotal() + 
+				" \n A área do terceiro triangulo é: " + t3.getTotal());
+		System.out.println("----------------------------");
+	}
 }
